ecd192b619898e75a6d57b482a5d7b0b467f1fb1
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-05-02 18:10:36 +0600

    read from json and method names

--- src/kg/attractor/java/Main.java
+++ src/kg/attractor/java/Main.java
@@ -1,21 +1,52 @@
 package kg.attractor.java;
 
-import kg.attractor.java.lesson.MovieCollection;
+import kg.attractor.java.homework.RestaurantOrders;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // это для занятия
-        var movieCollection = MovieCollection.readFromJson();
+        //   var movieCollection = MovieCollection.readFromJson();
 
         // это для домашки
         // выберите любое количество заказов, какое вам нравится.
 
-        //var orders = RestaurantOrders.read("orders_100.json").getOrders();
+        // var orders = RestaurantOrders.read("orders_100.json").getOrders();
         //var orders = RestaurantOrders.read("orders_1000.json").getOrders();
         //var orders = RestaurantOrders.read("orders_10_000.json").getOrders();
-
+        // var totalOrder = RestaurantOrders.read("orders_100.json").printOrderSum();
+        RestaurantOrders ro = new RestaurantOrders("orders_100.json");
+        ro.printOrderSum();
+        System.out.println();
+        ro.printList();
+        System.out.println();
+        ro.printMaxOrder();
+        System.out.println();
+        ro.printMinOrder();
+        System.out.println();
+        ro.printHomeMade();
+        System.out.println();
+        ro.printProfitHome();
+        System.out.println();
+        ro.betweenMinAndMax();
+        System.out.println();
+        ro.printAllSum();
+        System.out.println();
+        ro.printDistinctEmail();
+        System.out.println();
+        ro.uniqueOrderers();
+        System.out.println();
+        ro.totalSumOfUniqueOrderers();
+        System.out.println();
+        ro.totalSumOfUniqueOrderersMax();
+        System.out.println();
+        ro.totalSumOfUniqueOrdererMin();
+        System.out.println();
+        ro.productQuantity();
+        System.out.println();
+        ro.listEmail();
         // протестировать ваши методы вы можете как раз в этом файле (или в любом другом, в котором вам будет удобно)
     }
 }
+
